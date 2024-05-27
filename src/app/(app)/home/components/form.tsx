f4f62475f2f0a74d7be1794444470c35c5c4b381
John P <johnrprutton@gmail.com>
2024-05-27 14:22:34 +0200

    feat: add image preview

--- src/app/(app)/home/components/form.tsx
+++ src/app/(app)/home/components/form.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { useRef } from "react"
+import Image from "next/image"
+import { useRef, useState } from "react"
 
 import { useActions } from "ai/rsc"
-import { LucidePaperclip, LucideSend } from "lucide-react"
+import { LucidePaperclip, LucideSend, LucideXCircle } from "lucide-react"
 import { nanoid } from "nanoid"
 
 import { Button } from "@/components/ui/button"
@@ -19,6 +20,7 @@ export function Form({
 }) {
   const { continueConversation } = useActions()
   const imgSelectorRef = useRef<HTMLInputElement>(null)
+  const [imgBase64Url, setImgBase64Url] = useState<string | null>(null)
 
   const formAction = async (formData: FormData) => {
     const userInput = {
@@ -48,31 +50,66 @@ export function Form({
     ])
   }
 
+  const handleImageOnChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0]
+
+    if (!file) return setImgBase64Url(null)
+
+    const url = Buffer.from(await file.arrayBuffer()).toString("base64")
+    setImgBase64Url(`data:image/png;base64,${url}`)
+  }
+
   return (
-    <form className="flex flex-row gap-2" action={formAction}>
-      <>
-        <Button
-          variant="outline"
-          type="button"
-          onClick={() => imgSelectorRef.current?.click()}
-        >
-          <LucidePaperclip />
+    <div>
+      {imgBase64Url && (
+        <div className="relative w-fit">
+          <Image
+            src={imgBase64Url}
+            alt="preview of user image"
+            width={128}
+            height={128}
+            className="mb-2 rounded"
+          />
+
+          <Button
+            variant="ghost"
+            onClick={() => setImgBase64Url(null)}
+            className="absolute right-0 top-0 -translate-y-1/4 translate-x-full"
+            size="icon"
+          >
+            <LucideXCircle />
+          </Button>
+        </div>
+      )}
+
+      <form className="flex flex-row gap-2" action={formAction}>
+        <>
+          <Button
+            variant="outline"
+            type="button"
+            onClick={() => imgSelectorRef.current?.click()}
+          >
+            <LucidePaperclip />
+          </Button>
+
+          <input
+            ref={imgSelectorRef}
+            name="fileInput"
+            type="file"
+            hidden
+            accept="image/*"
+            onChange={handleImageOnChange}
+          />
+        </>
+
+        <Input name="textInput" type="text" />
+
+        <Button type="submit">
+          <LucideSend />
         </Button>
-
-        <input
-          ref={imgSelectorRef}
-          name="fileInput"
-          type="file"
-          hidden
-          accept="image/*"
-        />
-      </>
-
-      <Input name="textInput" type="text" />
-
-      <Button type="submit">
-        <LucideSend />
-      </Button>
-    </form>
+      </form>
+    </div>
   )
 }
