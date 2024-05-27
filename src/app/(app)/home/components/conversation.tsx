a97bd36022825cda126aa0c1bc883e8e279401cc
John P <johnrprutton@gmail.com>
2024-05-27 21:37:09 +0200

    feat: scroll to bottom of chat on render

--- src/app/(app)/home/components/conversation.tsx
+++ src/app/(app)/home/components/conversation.tsx
@@ -1,12 +1,24 @@
+import { useEffect, useRef } from "react"
+
 import { ClientMessage } from "../types"
+import { MessageBubble } from "./message-bubble"
 
 export function Conversation({
   conversation
 }: {
   conversation: ClientMessage[]
 }) {
+  const scrollRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+    }
+  })
   return (
-    <div className="-mt-8 space-y-4 overflow-y-scroll pb-4 pt-10">
+    <div
+      ref={scrollRef}
+      className="-mt-8 space-y-4 overflow-y-scroll pb-4 pt-10"
+    >
       {conversation.map((message: ClientMessage) => message.display)}
     </div>
   )
