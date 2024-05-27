1ea011079a603b03ceec70dad1e89facbbb85b91
John P <johnrprutton@gmail.com>
2024-05-27 09:31:28 +0200

    refactor: define provider in layout. needs to be server only anyway

--- src/app/(app)/home/components/index.tsx
+++ src/app/(app)/home/components/index.tsx
@@ -1,2 +1,1 @@
 export * from "./conversation"
-export * from "./providers"
--- src/app/(app)/home/layout.tsx
+++ src/app/(app)/home/layout.tsx
@@ -1,4 +1,15 @@
-import { AI } from "./components/providers"
+import { createAI } from "ai/rsc"
+
+import { continueConversation } from "./actions"
+import { ClientMessage, ServerMessage } from "./types"
+
+const AI = createAI<ServerMessage[], ClientMessage[]>({
+  actions: {
+    continueConversation
+  },
+  initialAIState: [],
+  initialUIState: []
+})
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <AI>{children}</AI>
