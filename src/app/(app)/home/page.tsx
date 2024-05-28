465b2573be860d9e0db5732d6ac656e5a120e566
John P <johnrprutton@gmail.com>
2024-05-28 13:49:40 +0200

    fix: hide page scroll

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const [conversation, setConversation] = useUIState()
 
   return (
-    <div className="flex flex-grow flex-col justify-between overflow-scroll">
+    <div className="flex flex-grow flex-col justify-between overflow-y-hidden">
       <Conversation conversation={conversation} />
 
       <Form setConversation={setConversation} />
