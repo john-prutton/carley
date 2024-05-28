c6204f0ca30e674b6f384e125fdf672a5f2ec4b3
John P <johnrprutton@gmail.com>
2024-05-28 14:23:27 +0200

    feat: add urlFromBase util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function urlFromBase(url: string) {
+  return new URL(url, process.env.BASE_URL!)
 }
