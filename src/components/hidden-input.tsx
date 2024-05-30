ae85f2fdff7b2a8ddfbedec3f1ff67ea79582caa
John P <johnrprutton@gmail.com>
2024-05-30 10:22:48 +0200

    refactor: style hidden input button better

--- src/components/hidden-input.tsx
+++ src/components/hidden-input.tsx
@@ -11,13 +11,18 @@ export function HiddenInput(props: InputProps) {
   const [pressed, setPressed] = useState(false)
 
   return (
-    <div className="flex flex-row gap-1">
+    <div className="relative">
       <Input
         placeholder="********"
         {...props}
         type={pressed ? "text" : "password"}
       />
-      <Toggle pressed={pressed} onPressedChange={setPressed}>
+      <Toggle
+        pressed={pressed}
+        onPressedChange={setPressed}
+        className="absolute right-0 top-1/2 -translate-y-1/2 text-muted-foreground"
+        tabIndex={-1}
+      >
         {!pressed ? <LucideEye /> : <LucideEyeOff />}
       </Toggle>
     </div>
