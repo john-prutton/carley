0869c0ed073a784340944cb49e5fe05dbd3de603
John P <johnrprutton@gmail.com>
2024-06-18 17:26:41 +0200

    feat: new hint for meal history functionality

--- src/app/(app)/home/lib/components/faq.tsx
+++ src/app/(app)/home/lib/components/faq.tsx
@@ -1,19 +1,43 @@
-import { LucideCamera } from "lucide-react"
+import { LucideAreaChart, LucideCamera, LucideProps } from "lucide-react"
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { cn } from "@/lib/utils/cn"
 
 export function FAQ({ className }: { className?: string }) {
   return (
-    <Card className={className}>
+    <div className={cn("space-y-4", className)}>
+      <FAQCard
+        icon={LucideCamera}
+        title="Analyze a Photo"
+        content="Take a photo of your meal and I'll analyze it! Feel free to write down any notes about the meal, such as the ingredients you used, or if you shared it with someone. This will all be taken into account."
+      />
+
+      <FAQCard
+        icon={LucideAreaChart}
+        title="View Meal History"
+        content="View a history of all the meals you've analyzed. You can see a breakdown of the nutritional information for each meal, as well as any notes you've written down."
+      />
+    </div>
+  )
+}
+
+function FAQCard({
+  icon: Icon,
+  title,
+  content
+}: {
+  icon: React.FC<LucideProps>
+  title: string
+  content: string
+}) {
+  return (
+    <Card>
       <CardHeader className="flex flex-row items-center gap-4">
-        <LucideCamera className="size-12 stroke-primary" />
-        <CardTitle>Analyze a Photo</CardTitle>
+        <Icon className="size-12 stroke-primary" />
+        <CardTitle>{title}</CardTitle>
       </CardHeader>
-      <CardContent>
-        Take a photo of your meal and I&apos;ll analyze it! Feel free to write
-        down any notes about the meal, such as the ingredients you used, or if
-        you shared it with someone. This will all be taken into account.
-      </CardContent>
+
+      <CardContent>{content}</CardContent>
     </Card>
   )
 }
