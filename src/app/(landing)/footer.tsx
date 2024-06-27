6ce71df644fc6eeccdb09c47ba18b3a51eaaba3a
John P <johnrprutton@gmail.com>
2024-06-27 16:22:37 +0200

    fix: center footer

--- src/app/(landing)/footer.tsx
+++ src/app/(landing)/footer.tsx
@@ -5,13 +5,15 @@ import { Button } from "@/components/ui/button"
 export function Footer() {
   return (
     <footer className="mt-16 flex items-center justify-center gap-2 bg-gray-100 py-16">
-      <Button asChild variant={"ghost"} className="w-fit px-0">
-        <Link href="https://github.com/john-prutton" target="_blank">
-          John Prutton
-        </Link>
-      </Button>
+      <div className="flex w-full">
+        <Button asChild variant={"ghost"} className="ml-auto w-fit px-0">
+          <Link href="https://github.com/john-prutton" target="_blank">
+            John Prutton
+          </Link>
+        </Button>
+      </div>
       <span className="text-2xl">🍍</span>
-      <span>2024</span>
+      <span className="w-full">2024</span>
     </footer>
   )
 }
