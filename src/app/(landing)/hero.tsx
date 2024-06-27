f77f706164c2a6d16b3769b0f18302333e3fc06a
John P <johnrprutton@gmail.com>
2024-06-27 14:55:07 +0200

    feat: add features to homepage

---
+++ src/app/(landing)/hero.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+
+import { SplashLogo } from "@/components/layout/splash-logo"
+import { Button } from "@/components/ui/button"
+
+export function Hero() {
+  return (
+    <section className="group mx-auto flex w-fit flex-col items-center gap-y-8 py-32">
+      <SplashLogo />
+
+      <h1 className="text-2xl tracking-wider">
+        Effortlessly track your nutrition
+      </h1>
+
+      <Button
+        asChild
+        className="w-[150%] max-w-[90svw] animate-pulse py-6 text-xl group-hover:animate-none"
+      >
+        <Link href="/home">Get Started</Link>
+      </Button>
+    </section>
+  )
+}
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,20 +1,61 @@
-import Link from "next/link"
+import mockupAnalyzeBrownRice from "@/../public/img/mockups/analyze-brown-rice.png"
+import mockupAnalyzeToastedSandwich from "@/../public/img/mockups/analyze-toasted-sandwich.png"
+import mockupMealHistoryDetailed from "@/../public/img/mockups/meal-history-detailed.png"
+import mockupMealHistoryZoomed from "@/../public/img/mockups/meal-history-zoomed.png"
 
-import { SplashLogo } from "@/components/layout/splash-logo"
-import { Button } from "@/components/ui/button"
+import { Feature } from "./feature"
+import { Hero } from "./hero"
 
 export default function Home() {
   return (
-    <div className="grid h-svh place-content-center">
-      <main className="group flex w-fit flex-col items-center gap-y-8">
-        <SplashLogo />
-        <Button
-          asChild
-          className="w-[150%] max-w-[90svw] animate-pulse py-6 text-xl group-hover:animate-none"
-        >
-          <Link href="/home">Get Started</Link>
-        </Button>
-      </main>
-    </div>
+    <main>
+      <Hero />
+
+      <div className="mx-auto max-w-5xl px-8">
+        <section>
+          <h2 className="mb-8 translate-x-4 text-2xl font-bold">
+            ✨ v0.1.0 Features
+          </h2>
+
+          <div className="space-y-16">
+            <Feature
+              features={[
+                {
+                  icon: "📸",
+                  title: "Analyze a Photograph",
+                  description:
+                    "Simply snap a photo of your meal, and Carley will analyze the image to extract detailed nutritional information, including calories, protein, carbs, and fat."
+                },
+                {
+                  icon: "📝",
+                  title: "Add Notes",
+                  description:
+                    "Add optional text describing your meal to increase Carley's response accuracy. You could include ingredients that aren't visible, or say whether you shared or finished the meal."
+                }
+              ]}
+              mockups={[mockupAnalyzeBrownRice, mockupAnalyzeToastedSandwich]}
+            />
+
+            <Feature
+              features={[
+                {
+                  icon: "📈",
+                  title: "Visualize Your Progress",
+                  description:
+                    "Get a comprehensive view of your nutrition over time with interactive charts that display your daily totals."
+                },
+                {
+                  icon: "🗄️",
+                  title: "Cloud Storage",
+                  description:
+                    "All your data is securely stored in the cloud, so you can access it from any device."
+                }
+              ]}
+              mockups={[mockupMealHistoryZoomed, mockupMealHistoryDetailed]}
+            />
+          </div>
+        </section>
+      </div>
+    </main>
   )
 }
