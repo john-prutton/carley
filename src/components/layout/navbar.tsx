06f9c3df9fc2584f1873a3e137283c72a015db53
John P <johnrprutton@gmail.com>
2024-06-28 15:29:23 +0200

    feat: add link to nav logo

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link"
+
 import { ProfileDropdownMenu } from "../profile-dropdown-menu"
 
 export function Navbar() {
   return (
     <nav className="flex flex-row items-center justify-between rounded-b-xl border border-t-transparent bg-white p-2 drop-shadow">
-      <div className="flex flex-row items-center ">
+      <Link href={"/"} className="flex flex-row items-center ">
         <span className="mr-1 scale-150 text-lg">🍍</span>
         <span className="scale-90 text-xl font-black leading-[100%]">
           Carley
         </span>
-      </div>
+      </Link>
 
       <ProfileDropdownMenu />
     </nav>
