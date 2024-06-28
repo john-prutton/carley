b9eaab0f7d5e1b5d62e98eced2170625c940c565
John P <johnrprutton@gmail.com>
2024-06-28 13:59:27 +0200

    feat: add metadata and viewport to pages

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -2,7 +2,7 @@ import { ProfileDropdownMenu } from "../profile-dropdown-menu"
 
 export function Navbar() {
   return (
-    <nav className="flex flex-row items-center justify-between rounded-b-xl border bg-white p-2 drop-shadow">
+    <nav className="flex flex-row items-center justify-between rounded-b-xl border border-t-transparent bg-white p-2 drop-shadow">
       <div className="flex flex-row items-center ">
         <span className="mr-1 scale-150 text-lg">🍍</span>
         <span className="scale-90 text-xl font-black leading-[100%]">
